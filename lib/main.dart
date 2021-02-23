1b2ea279472e126ff2bdd925f7cdeba9b657834b
manirahmanzadeh <mani.rahmanzade@gmail.com>
2021-02-23 04:53:01 +0330

    Form Demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,89 @@
+
 import 'package:flutter/material.dart';
 
 void main()=> runApp(
-  MaterialApp(
-    home: MyHomePage(),
-  )
+    MaterialApp(
+      home: MyHomePage(),
+    )
 );
 
 class MyHomePage extends StatelessWidget {
+
+  final formKey = GlobalKey<FormState>();
+
+  SignUpModel signUpModel = SignUpModel();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      floatingActionButton: Builder(
-          builder: (BuildContext ctx)=> FloatingActionButton(
-            onPressed: (){
-              Scaffold.of(ctx).showSnackBar(
-                  SnackBar(content: Text("Sample SnackBar"))
-              );
-            },
+      body: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                    labelText: "email"
+                ),
+                onSaved: (v){
+                  signUpModel.email = v;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    labelText: "first name"
+                ),
+                onSaved: (v){
+                  signUpModel.firstName = v;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    labelText: "last name"
+                ),
+                onSaved: (v){
+                  signUpModel.lastName = v;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    labelText: "city"
+                ),
+                onSaved: (v){
+                  signUpModel.city = v;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    labelText: "password"
+                ),
+                onSaved: (v){
+                  signUpModel.password = v;
+                },
+              ),
+              Builder(builder: (BuildContext ctx)=> RaisedButton(
+                  child: Text("Sign Up"),
+                  onPressed: () {
+                    formKey.currentState.save();
+                  }
+              ))
+
+            ],
           )
-      )
+      ),
     );
   }
+
+
 }
+
+class SignUpModel{
+  String email;
+  String firstName;
+  String lastName;
+  String city;
+  String password;
+  SignUpModel({this.email, this.city, this.firstName, this.lastName, this.password});
+}
+
 
