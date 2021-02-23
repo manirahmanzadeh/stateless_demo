b5b414b65e941ef8538e00d19175d6fc7d39a058
manirahmanzadeh <mani.rahmanzade@gmail.com>
2021-02-23 04:50:51 +0330

    SnackBar Demo

--- lib/main.dart
+++ lib/main.dart
@@ -7,82 +7,20 @@ void main()=> runApp(
 );
 
 class MyHomePage extends StatelessWidget {
-
-  final formKey = GlobalKey<FormState>();
-
-  SignUpModel signUpModel = SignUpModel();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Form(
-        key: formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: "email"
-                ),
-                onSaved: (v){
-                  signUpModel.email = v;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: "first name"
-                ),
-                onSaved: (v){
-                  signUpModel.firstName = v;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: "last name"
-                ),
-                onSaved: (v){
-                  signUpModel.lastName = v;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: "city"
-                ),
-                onSaved: (v){
-                  signUpModel.city = v;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: "password"
-                ),
-                onSaved: (v){
-                  signUpModel.password = v;
-                },
-              ),
-              Builder(builder: (BuildContext ctx)=> RaisedButton(
-                  child: Text("Sign Up"),
-                  onPressed: () {
-                    formKey.currentState.save();
-                  }
-              ))
-
-            ],
+      floatingActionButton: Builder(
+          builder: (BuildContext ctx)=> FloatingActionButton(
+            onPressed: (){
+              Scaffold.of(ctx).showSnackBar(
+                  SnackBar(content: Text("Sample SnackBar"))
+              );
+            },
           )
-      ),
+      )
     );
   }
-
-
 }
-
-class SignUpModel{
-  String email;
-  String firstName;
-  String lastName;
-  String city;
-  String password;
-  SignUpModel({this.email, this.city, this.firstName, this.lastName, this.password});
-}
-
 
