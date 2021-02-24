10dfc7c3947135fb794052d74151dfb65ce66161
manirahmanzadeh <mani.rahmanzade@gmail.com>
2021-02-24 04:32:01 +0330

    Stateless Demo

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,21 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My First App"),
-        ),
-        body: Center(
-          child: Text("I love Flutter"),
-        ),
-      ),
+      home: MyHomePage()
     )
   );
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+      ),
+      body: Center(
+        child: Text("I love Flutter"),
+      ),
+    );
+  }
 }
